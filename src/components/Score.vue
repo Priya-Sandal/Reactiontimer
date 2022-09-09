17b205e41d816f532b382f6cce24bef439722eeb
unknown <add>
2022-09-09 13:41:55 +0530

    add

---
+++ src/components/Score.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="score">
+        <p id="details">{{ clickvar.details }}</p>
+        <p class="span-results">
+            <b>Your High Score</b>
+            <p id="highscore">{{ clickvar.highscore }}</p>
+        </p>
+    </div>
+    </template>
+    
+    <script>
+    export default {
+        data() {
+            return {}
+        },
+        props: {
+            clickvar: Object
+        }
+    }
+    </script>
